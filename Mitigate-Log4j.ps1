58f8619b389eaf902fb06e62000de6663e6787e5
Michael Bennett <m.s.bennett@outlook.com>
2021-12-23 14:55:52 +1300

    Reworked the file
    Added in the scanning to pass over to the mitigation phase.
    You can choose to run the mitigation after the scan, or list the files for reporting.

--- Mitigate-Log4j.ps1
+++ Mitigate-Log4j.ps1
@@ -17,84 +17,110 @@ if ((CheckAdmin) -eq $false) {
     Exit
 }
 
-# Check if we have 7-Zip installed
-$check7zip = Test-Path -Path 'c:\Program Files\7-Zip\7z.exe'
+# The command below will get the details of Disks and select the Device ID for use below
+$Drives = Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3' | 
+    Select-Object DeviceID
 
-if ($check7zip -eq $False) {
-    # Borrowed from https://gist.github.com/SomeCallMeTom/6dd42be6b81fd0c898fa9554b227e4b4 and tweaked with a couple of try/catches and error catching
-    Write-Host '7-Zip is not installed'
-    $install7zip = Read-Host 'Would you like to download and install it (Y/n)'
-    If ($install7zip -eq 'Y') {
-        $dlurl = 'https://7-zip.org/' + (Invoke-WebRequest -UseBasicParsing -Uri 'https://7-zip.org/' | Select-Object -ExpandProperty Links | Where-Object { ($_.outerHTML -match 'Download') -and ($_.href -like 'a/*') -and ($_.href -like '*-x64.exe') } | Select-Object -First 1 | Select-Object -ExpandProperty href)
-        # modified to work without IE
-        # above code from: https://perplexity.nl/windows-powershell/installing-or-updating-7-zip-using-powershell/
-        $installerPath = Join-Path $env:TEMP (Split-Path $dlurl -Leaf)
-        Try { 
-            Invoke-WebRequest $dlurl -OutFile $installerPath 
+# Scan each disk on the system and look for the particular string
+$vulnFiles = @()
+Write-Host 'Scanning in progress. Please Wait'
+foreach ($drive in $Drives) {
+    $drive = $drive.DeviceID
+    $vulnFiles += Get-ChildItem "$drive\" -Filter log4j-core*.jar -Recurse -ErrorAction SilentlyContinue | 
+        ForEach-Object { Select-String 'JndiLookup.class' $_ } | 
+        Select-Object -ExpandProperty Path
+}
+
+if ($vulnFiles -ne $null) {
+    # Output the files found
+    $vulnFules
+
+    $Mitigate = Read-Host 'Would you like to Mitigate the Log4j Files? (Y/n)'
+
+    if ($Mitigate -eq 'Y') {
+
+        # Check if we have 7-Zip installed
+        $check7zip = Test-Path -Path 'c:\Program Files\7-Zip\7z.exe'
+
+        if ($check7zip -eq $False) {
+            # Borrowed from https://gist.github.com/SomeCallMeTom/6dd42be6b81fd0c898fa9554b227e4b4 and tweaked with a couple of try/catches and error catching
+            Write-Host '7-Zip is not installed'
+            $install7zip = Read-Host 'Would you like to download and install it (Y/n)'
+            If ($install7zip -eq 'Y') {
+                Try {
+                    $dlurl = 'https://7-zip.org/' + (Invoke-WebRequest -UseBasicParsing -Uri 'https://7-zip.org/' -ErrorAction Stop | Select-Object -ExpandProperty Links | Where-Object { ($_.outerHTML -match 'Download') -and ($_.href -like 'a/*') -and ($_.href -like '*-x64.exe') } | Select-Object -First 1 | Select-Object -ExpandProperty href)
+                }
+                Catch {
+                    $ErrorMessage = $_.Exception.Message
+                    Write-Warning "$ErrorMessage"
+                    Write-Host 'Please download and install 7-Zip'
+                    Write-Host 'https://www.7-zip.org/download.html'
+                }
+                # Modified to work without IE
+                # Above code from: https://perplexity.nl/windows-powershell/installing-or-updating-7-zip-using-powershell/
+                if ($null -eq $ErrorMessage) {
+                    # Create the Installer path
+                    $installerPath = Join-Path $env:TEMP (Split-Path $dlurl -Leaf)
+                    # Download the file
+                    Invoke-WebRequest $dlurl -OutFile $installerPath
+                    # Install the file
+                    $ExitCode = (Start-Process -FilePath $installerPath -Args '/S' -Verb RunAs -Wait -PassThru).ExitCode
+                    if ($ExitCode -eq 0) {
+                        Write-Host 'success!' -ForegroundColor Green
+                        $check7zip = Test-Path -Path 'c:\Program Files\7-Zip\7z.exe'
+                    }
+                    else {
+                        Write-Host "failed. There was a problem installing 7-Zip. Exit code $ExitCode." -ForegroundColor Red
+                    }
+                    Remove-Item $installerPath
+                }
+            }
+    
         }
-        catch {
-            $ErrorMessage = $_.Exception.Message
-            Write-Warning "$ErrorMessage"
+
+        # If 7-Zip is installed, then lets go!
+        if ($check7zip -eq $True) {
+            Foreach ($vulnFile in $vulnFiles) {
+                # Start removing JndiLookup.class from each of the files we discovered
+                Write-Host "Removing JndiLookup.class from $vulnfile ..." -NoNewline
+                try {
+                    $7zDelArg = "d `"$vulnFile`" org/apache/logging/log4j/core/lookup/JndiLookup.class"
+                    Start-Process -NoNewWindow -FilePath 'c:\Program Files\7-Zip\7z.exe' -Args $7zDelArg -Verb RunAs -Wait -ErrorAction Stop | Out-Null
+                    Write-Host ' Done' -ForegroundColor Green
+                }
+                catch {
+                    Write-Host ' Error removing file' -ForegroundColor Red
+                    $ErrorMessage = $_.Exception.Message
+                    Write-Warning "$ErrorMessage"
+                }
+           
+                # Check the file to see if we actually deleted it
+                Write-Host 'Doing a sanity check on the file...' -NoNewline
+                $check = & 'c:\Program Files\7-Zip\7z.exe' l -r "$vulnFile" | findstr JndiLookup
+                if ($null -eq $check) {
+                    Write-Host '    File still exists, please try again' -ForegroundColor Red
+                    $check
+                }
+                else {
+                    Write-Host '    File has been deleted' -ForegroundColor Green
+                }  
+            }
+        }
+        # You are going to have to manually download and install 7-Zip
+        else {
             Write-Host 'Please download and install 7-Zip'
             Write-Host 'https://www.7-zip.org/download.html'
         }
-        if ($ErrorMessage -ne $null) {
-            $ExitCode = (Start-Process -FilePath $installerPath -Args '/S' -Verb RunAs -Wait -PassThru).ExitCode
-            if ($ExitCode -eq 0) {
-                Write-Host 'success!' -ForegroundColor Green
-            }
-            else {
-                Write-Host "failed. There was a problem installing 7-Zip. Exit code $ExitCode." -ForegroundColor Red
-            }
-            Remove-Item $installerPath
+    }
+    else {
+        Write-Host "List of vulnerable files are as follows `n"
+        Foreach ($vulnFile in $vulnFiles) {
+            Write-Host "$vulnFile"
         }
     }
-    $check7zip = Test-Path -Path 'c:\Program Files\7-Zip\7z.exe'
 }
-
-# If 7-Zip is installed, then lets go!
-if ($check7zip -eq $True) {
-    Write-Host 'Gathering Drive Information...'
-    $disks = Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3'
-    foreach ($disk in $disks) {
-
-        $diskname = "$($volume.DeviceID)\"
-        
-        Write-Host "`n** Checking $diskname... ** `n"
-     
-        Write-Host '** Searching for log4j-core*.jar files... ** `n'
-        Get-ChildItem -Path $diskname -Filter log4j-core*.jar -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
-            $check = $null
-            Write-Host "`n`n *** File found $($_.FullName) ***"
-            
-            # Find any log4j-core*.jar files and remove org/apache/logging/log4j/core/lookup/JndiLookup.class with zip.exe -q -d command.
-            Write-Host "Removing JndiLookup.class from `"$($_.FullName)`"..." -NoNewline
-            try {
-                &  'c:\Program Files\7-Zip\7z.exe' d "$($_.FullName)" 'org/apache/logging/log4j/core/lookup/JndiLookup.class' | Out-Null
-                Write-Host ' Done' -ForegroundColor Green
-            }
-            catch {
-                Write-Host ' Error removing file' -ForegroundColor Red
-            }
-           
-            # Use unzip -l (List) | findstr JndiLookup sanity check on the file to make sure that JndiLookup.Class has been removed.
-            Write-Host 'Doing a sanity check on the file to make sure that JndiLookup.Class has been removed'
-            $check = & 'c:\Program Files\7-Zip\7z.exe' l -r "$($_.FullName)" | findstr JndiLookup
-            if ($check -ne $null) {
-                Write-Host '    Looks like the JndiLookup.class file still exists, please try again'
-                $check
-            }
-            else {
-                Write-Host "    The file has been removed `"$($_.FullName)`""
-            }       
-        }
-        Write-Host "** Finished checking $diskname **`n"     
-    }
-}
-# You are going to have to manually download and install 7-Zip
 else {
-    Write-Host 'Please download and install 7-Zip'
-    Write-Host 'https://www.7-zip.org/download.html'
+    Write-Host 'No files found'
 }
 
 
